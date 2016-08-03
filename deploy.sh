f1ee6ea5a505b29323d8b544afb5b51e1985708a
xmdlut <854373728@qq.com>
2016-08-03 14:46:20 +0800

    使用rootpom

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 set -e
-
+mvn -U clean install
 TOMCAT_PATH=/usr/local/Cellar/tomcat/8.5.3/libexec/webapps
 rm -rf $TOMCAT_PATH/eva-web-0.0.1-SNAPSHOT
 cp -rf ./target/eva-web-0.0.1-SNAPSHOT $TOMCAT_PATH
