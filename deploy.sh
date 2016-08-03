a6af2a14570c18c1bd312d075b629f504ab8d7a1
xmdlut <854373728@qq.com>
2016-08-03 13:26:01 +0800

    增加本地部署脚本 发布到tomcat中

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+set -x
+set -e
+
+TOMCAT_PATH=/usr/local/Cellar/tomcat/8.5.3/libexec/webapps
+rm -rf $TOMCAT_PATH/eva-web-0.0.1-SNAPSHOT
+cp -rf ./target/eva-web-0.0.1-SNAPSHOT $TOMCAT_PATH
